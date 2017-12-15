acf181eb4473c4fd3697881cd624039dc122377e
Michael Lange <michael.lange@imperial.ac.uk>
2017-12-15 07:19:21 +0000

    Arguments: Add runtime value derivation from user input
    Includes a small hack to make the `profiler` work with current
    implementation.

--- devito/types.py
+++ devito/types.py
@@ -185,6 +185,15 @@ class AbstractSymbol(sympy.Symbol, CachedSymbol):
         raise NotImplementedError('%s does not provide any default arguments' %
                                   self.__class__)
 
+    def argument_values(self, **kwargs):
+        """
+        Returns a map of argument values after evaluating user input.
+
+        :param kwargs: Dictionary of user-provided argument overrides.
+        """
+        raise NotImplementedError('%s does not provide argument value derivation' %
+                                  self.__class__)
+
 
 class Symbol(AbstractSymbol):
 
@@ -331,6 +340,14 @@ class AbstractFunction(sympy.Function, CachedSymbol):
         raise NotImplementedError('%s does not provide any default arguments' %
                                   self.__class__)
 
+    def argument_values(self, **kwargs):
+        """
+        Returns a map of argument values after evaluating user input.
+
+        :param kwargs: Dictionary of user-provided argument overrides.
+        """
+        raise NotImplementedError('%s does not provide argument value derivation' %
+                                  self.__class__)
 
 class SymbolicData(AbstractFunction):
 
